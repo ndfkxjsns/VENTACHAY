d28444a2cc376768ab40776fc8481466d15835be
Axel098 <yanetaquino19770@gmail.com>
2025-04-30 23:52:03 -0600

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,9 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+
+// Tu configuración de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBftARJVB3Cq1jQoPXdb-ldF-_NEWnyO_8",
   authDomain: "tcservidor-b8874.firebaseapp.com",
@@ -8,3 +13,10 @@ const firebaseConfig = {
   appId: "1:1033968746876:web:d75c3247adc32f785864f9",
   measurementId: "G-R7L2183KKD"
 };
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app); // Inicializa Firestore
+
+export { db }; // Exporta la instancia de Firestore
